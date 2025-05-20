64e55eed880b26b475e0e3c6b5b28217b924bc26
kalobmj <kalobmurphyjones@gmail.com>
2025-05-20 14:59:36 -0400

    js comments

--- main/pig.js
+++ main/pig.js
@@ -13,11 +13,19 @@ let cellsize = diceLeft.height / 3;
 
 console.log({ cellsize });
 
+// can just have main dice color as one rect
+// then can draw dots using loops and arc
+
+// but idk because it might be easier to loop rect by rect and detrmine if cell needs to be filled with dot (idk)
+
 // color die (test)
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < cols; j++) {
     const isEven = (i + j) % 2 === 0;
-    const color = isEven ? "red" : "blue";
+
+    // colors are placeholder, just testing colors
+    // const color = isEven ? "#00637C" : "#2A2B2D";
+    const color = '#00637C'
 
     console.log({ isEven });
     console.log({ color });
@@ -25,12 +33,19 @@ for (let i = 0; i < rows; i++) {
     canvas1.fillStyle = color;
     canvas1.fillRect(j * cellsize, i * cellsize, cellsize, cellsize);
 
+    canvas1.strokeStyle = "rgba(0, 0, 0, 0)";
+    canvas1.strokeRect(j * cellsize, i * cellsize, cellsize, cellsize)
+    
     // making circle (dot in dice)
     // canvas1.beginPath();
     // canvas1.arc(100, 75, 50, 0, 2 * Math.PI);
     // ctx.stroke();
-
+    
     canvas2.fillStyle = color;
     canvas2.fillRect(j * cellsize, i * cellsize, cellsize, cellsize);
+    
+    canvas2.strokeStyle = "rgba(1, 1, 1, 0)";
+    canvas2.strokeRect(j * cellsize, i * cellsize, cellsize, cellsize)
+
   }
 }
